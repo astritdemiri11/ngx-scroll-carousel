edb1aa770eb39c843616d90457801ae29b3bbf48
Astrit Demiri <astritdemiri11@gmail.com>
2023-02-13 11:06:30 +0100

    Render feature added

--- src/lib/components/carousel/carousel.component.ts
+++ src/lib/components/carousel/carousel.component.ts
@@ -60,6 +60,68 @@ export class CarouselComponent implements OnInit, OnChanges, AfterContentInit, A
     this.subscriptions = [];
   }
 
+  render() {
+    if (!this.carouselItems || this.carouselItems.length === 0 || !this.carouselContainer) {
+      return;
+    }
+
+    if (this.interval$) {
+      this.interval$.unsubscribe();
+    }
+
+    if (this.data.verticalVersion) {
+      this.carouselContainer.nativeElement.scrollTop = 0;
+    } else {
+      this.carouselContainer.nativeElement.scrollLeft = 0;
+    }
+
+    this.itemsLength = this.carouselItems.length;
+
+    let itemsLength = this.data.items;
+
+    if (itemsLength > this.itemsLength) {
+      itemsLength = this.itemsLength;
+    }
+
+    let itemsGap = 0;
+
+    if (this.data.itemsGapPX) {
+      itemsGap = this.data.itemsGapPX;
+    }
+
+    this.index = 0;
+
+    this.carouselItems.forEach((carouselItem: ElementRef, index: number) => {
+      const elem = carouselItem.nativeElement;
+
+      if (itemsLength > 1) {
+        this.renderer2.setStyle(elem, 'flex', `0 0 calc(${(100 / itemsLength)}% - ${itemsGap * (itemsLength - 1) / itemsLength}px)`);
+      } else {
+        this.renderer2.setStyle(elem, 'flex', `0 0 100%`);
+      }
+
+      if (this.data.itemsGapPX && index + 1 < this.itemsLength) {
+        if (this.data.verticalVersion) {
+          this.renderer2.setStyle(elem, 'margin-bottom', this.data.itemsGapPX + 'px');
+        } else {
+          this.renderer2.setStyle(elem, 'margin-right', this.data.itemsGapPX + 'px');
+        }
+      }
+    });
+
+    if (this.data.autoplay) {
+      this.startCarousel();
+    }
+
+    if (this.itemsLength > 0) {
+      const length = this.itemsLength - Math.floor(this.data.items) + 1;
+
+      if (length > 1) {
+        this.controls = Array(length).fill(0).map((_x, i) => i);
+      }
+    }
+  }
+
   onPrev(event: Event) {
     event.preventDefault();
 
@@ -205,61 +267,7 @@ export class CarouselComponent implements OnInit, OnChanges, AfterContentInit, A
     }
 
     if (this.configs && (!this.configs.omitChanges || !this.changesApplied)) {
-      if (this.interval$) {
-        this.interval$.unsubscribe();
-      }
-
-      if (this.data.verticalVersion) {
-        this.carouselContainer.nativeElement.scrollTop = 0;
-      } else {
-        this.carouselContainer.nativeElement.scrollLeft = 0;
-      }
-
-      this.itemsLength = this.carouselItems.length;
-
-      let itemsLength = this.data.items;
-
-      if (itemsLength > this.itemsLength) {
-        itemsLength = this.itemsLength;
-      }
-
-      let itemsGap = 0;
-
-      if (this.data.itemsGapPX) {
-        itemsGap = this.data.itemsGapPX;
-      }
-
-      this.index = 0;
-
-      this.carouselItems.forEach((carouselItem: ElementRef, index: number) => {
-        const elem = carouselItem.nativeElement;
-
-        if (itemsLength > 1) {
-          this.renderer2.setStyle(elem, 'flex', `0 0 calc(${(100 / itemsLength)}% - ${itemsGap * (itemsLength - 1) / itemsLength}px)`);
-        } else {
-          this.renderer2.setStyle(elem, 'flex', `0 0 100%`);
-        }
-
-        if (this.data.itemsGapPX && index + 1 < this.itemsLength) {
-          if (this.data.verticalVersion) {
-            this.renderer2.setStyle(elem, 'margin-bottom', this.data.itemsGapPX + 'px');
-          } else {
-            this.renderer2.setStyle(elem, 'margin-right', this.data.itemsGapPX + 'px');
-          }
-        }
-      });
-
-      if (this.data.autoplay) {
-        this.startCarousel();
-      }
-
-      if (this.itemsLength > 0) {
-        const length = this.itemsLength - Math.floor(this.data.items) + 1;
-
-        if (length > 1) {
-          this.controls = Array(length).fill(0).map((_x, i) => i);
-        }
-      }
+      this.render();
     }
 
     this.changesApplied = true;
